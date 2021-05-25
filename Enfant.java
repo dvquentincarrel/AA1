66e12f5ad3f8399250c9419039a1622f8c4a7c50
dvquentincarrel <78238441+dvquentincarrel@users.noreply.github.com>
2021-05-25 17:34:29 +0200

    update commentaires

--- Enfant.java
+++ Enfant.java
@@ -14,7 +14,6 @@ public class Enfant implements Serializable {
     private static final long serialVersionUID = 1L;  // nécessaire pour la sérialisation
     private final String nom;
     private HashMap<Seance,Jour> seances = new HashMap<Seance,Jour>();
-    //private ArrayList<Seance> seances = new ArrayList<Seance>(); // A DELETE
 
     public Enfant(String nom) {
         this.nom = nom;
@@ -24,11 +23,12 @@ public class Enfant implements Serializable {
         return this.nom;
     }
 
-    public Collection<Seance> getSeance() { //A REVOIR
+    public Collection<Seance> getSeance() {
         return seances.keySet();
     }
 
     public void setSeance(Seance seance) {
+        // Plutôt addSeance que setSeance, mais on se comprend.
         seances.put(seance,seance.getJour());      
     }
 
